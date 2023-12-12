7240853d6d193b9686fde30fd1332461b974d9c7
raufur <raufur.rahman@brilliant.com.bd>
2023-12-12 15:34:17 +0600

    output changes

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def home():
     app.logger.info(val)
     redis_client.set('visits', int(val) + 1)
 
-    return f"Visits  info: {val}"
+    return f"Number of total visits: {val}"
 
 
 if __name__ == '__main__':
