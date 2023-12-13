bce593e70950919eaa9e9d515b7ffa73699f7057
raufur <raufur.rahman@brilliant.com.bd>
2023-12-13 11:58:25 +0600

    new cmt

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def home():
     app.logger.info(val)
     redis_client.set('visits', int(val) + 1)
 
-    return f"total visits: {val}"
+    return f"Total visits: {val}"
 
 
 if __name__ == '__main__':
